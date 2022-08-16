09ad39d23ab3e6e559b0b926601b3570c0be953c
IuriiSer <yusergeevi4@gmail.com>
2022-08-16 22:19:09 +0300

    + yMap init builded on Promis + advsPage + advsRouter/Controller

---
+++ public/js/lib/initYMap.js
@@ -0,0 +1,32 @@
+function initYMap() {
+  return new Promise((res) => {
+    ymaps.ready(init);
+    function init() {
+      const myMap = new ymaps.Map('map', {
+        center: [55.76, 37.64],
+        zoom: 10,
+      }, {
+        searchControlProvider: 'yandex#search',
+      });
+      const objectManager = new ymaps.ObjectManager({
+        // Чтобы метки начали кластеризоваться, выставляем опцию.
+        clusterize: true,
+        // ObjectManager принимает те же опции, что и кластеризатор.
+        gridSize: 32,
+        clusterDisableClickZoom: true,
+      });
+
+      // Чтобы задать опции одиночным объектам и кластерам,
+      // обратимся к дочерним коллекциям ObjectManager.
+      objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+      objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+      myMap.geoObjects.add(objectManager);
+
+      yMap.map = myMap;
+      yMap.objectManager = objectManager;
+      res();
+    }
+  });
+}
+
+const yMap = { isInited: initYMap() };
