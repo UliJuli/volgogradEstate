5a49c08b0ffb25346f9fe2465900ce7df98db8a8
Julia <sizova.julia.1990@gmail.com>
2022-08-16 19:41:12 +0300

    release

--- src/views/components/Header.jsx
+++ src/views/components/Header.jsx
@@ -17,7 +17,7 @@ class Header extends React.PureComponent {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
+                  <a className="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="#">Category</a>
@@ -35,12 +35,12 @@ class Header extends React.PureComponent {
                 </li>
                 {!user && (
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Login</a>
+                  <a className="nav-link" href="/login">Login</a>
                 </li>
                 )}
                 {user && (
                 <li className="nav-item">
-                  <a className="nav-link" href="#">Logout</a>
+                  <a className="nav-link" href="/logout">Logout</a>
                 </li>
                 )}
               </ul>
