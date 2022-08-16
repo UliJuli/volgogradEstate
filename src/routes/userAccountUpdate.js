5c1dd470122611898f3302b50330df0d7202d86c
Julia <sizova.julia.1990@gmail.com>
2022-08-16 20:36:39 +0300

    fixed

--- src/routes/userAccountUpdate.js
+++ src/routes/userAccountUpdate.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const renderTemplate = require('../lib/renderTemplate');
-const ProfilePage = require('../views/pages/ProfilePage');
+const UserUpdate = require('../views/pages/UserUpdate');
 
 const { User, WishList } = require('../../db/models');
 
@@ -9,24 +9,10 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   try {
-    renderTemplate(Registr, { }, res);
+    renderTemplate(UserUpdate, { }, res);
   } catch (error) {
     console.log(error);
   }
 });
-
-// router.put('/', async (req, res) => {
-//   const login = req.session.user;
-//   const {
-//     firstName, lastName, phoneNumber, email, password,
-//   } = req.body;
-//   try {
-//     await User.update({
-//       firstName, lastName, phoneNumber, email, password,
-//     }, { where: { email: login } });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
 
 module.exports = router;
