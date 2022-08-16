6553c32114c511e54b329c3411d3d56d2326999b
IuriiSer <yusergeevi4@gmail.com>
2022-08-17 01:35:03 +0300

    + event listner for mapWindow + export data drom map window

--- public/js/lib/initYMap.js
+++ public/js/lib/initYMap.js
@@ -29,4 +29,29 @@ function initYMap() {
   });
 }
 
-const yMap = { isInited: initYMap() };
+function addNewObjects(_callBack) {
+  let eventListnerId;
+  const callBack = _callBack;
+  const objectsOnMap = []; // <- [1,2,3,4]
+  return async function (objects) {
+    await this.isInited;
+    objectsOnMap.push(...objects);
+    const obj = ymaps.geoQuery(objectsOnMap);
+    this.map.geoObjects.add(obj.clusterize());
+
+    const eventListner = this.map.events.add('boundschange', () => {
+      const visibleObjects = obj.searchInside(this.map);
+      this.visibleObjects = visibleObjects;
+      if (callBack) callBack();
+    });
+
+    if (eventListnerId) eventListner.types.boundschange[eventListnerId] = null;
+    eventListnerId = eventListner.types.boundschange.length - 2;
+  };
+}
+
+function callBack() {
+  console.log(yMap.visibleObjects);
+}
+
+const yMap = { isInited: initYMap(), addNewObjects: addNewObjects(callBack), visibleObjects: [] };
