090b99f941bc7a11f8c4833863ac97172c56f4ec
IuriiSer <yusergeevi4@gmail.com>
2022-08-17 16:35:45 +0300

    added middlwares for login/regin routers

--- src/middlewares/logInSessionHandler.js
+++ src/middlewares/logInSessionHandler.js
@@ -1,18 +1,14 @@
 const logInSessionWrapper = (req, res, next) => {
   const user = req.session?.user;
   if (user) {
+    console.log('~ user', user);
     res.locals.user = user;
-    // console.log('~ res.locals.user', res.locals.user);
-    next();
-    return;
   }
 
   const admin = req.session?.admin;
-  if (!admin) {
+  if (admin) {
+    console.log('~ admin', admin);
     res.locals.admin = admin;
-    // console.log('~ res.locals.admin', res.locals.admin);
-    next();
-    return;
   }
 
   next();
