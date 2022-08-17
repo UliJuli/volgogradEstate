3c2d768235f7a842fa5e397b008c861064865a4a
Vlad <vladvasenin@gmail.com>
2022-08-17 14:28:21 +0300

    somefixes

--- src/views/pages/MainPage.jsx
+++ src/views/pages/MainPage.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable react/jsx-no-comment-textnodes */
 const React = require('react');
 const Layout = require('../components/Layout');
 
@@ -14,32 +16,37 @@ module.exports = function Main({ title }) {
                 <option value="1">Квартира</option>
                 <option value="2">Комната</option>
               </select>
-            </div>
-            <div className="col-4">
-              <div className="form-check form-check-inline">
-                <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                <label className="form-check-label" htmlFor="inlineCheckbox1">1</label>
+              <div className="checkbox form-check form-check-inline">
+                <h8>Количество комнат :</h8>
+                <div className="boxcheck">
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
+                  <label className="form-check-label" htmlFor="inlineCheckbox1">1</label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
+                  <label className="form-check-label" htmlFor="inlineCheckbox2">2</label>
+                </div>
+                <div className="form-check form-check-inline">
+                  <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
+                  <label className="form-check-label" htmlFor="inlineCheckbox3">3</label>
+                </div>
+                </div>
               </div>
-              <div className="form-check form-check-inline">
-                <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                <label className="form-check-label" htmlFor="inlineCheckbox2">2</label>
+              <h8 className="aria-price">Диапозон цены:</h8>
+              <div className="price-container">
+                <div className="input-group mb-3">
+                  <span className="input-group-text">₽</span>
+                  <span className="input-group-text">0</span>
+                  <input type="text" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
+                </div>
+                <div className="input-group">
+                  <input type="text" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
+                  <span className="input-group-text">₽</span>
+                  <span className="input-group-text">50000</span>
+                </div>
               </div>
-              <div className="form-check form-check-inline">
-                <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
-                <label className="form-check-label" htmlFor="inlineCheckbox3">3</label>
-              </div>
             </div>
-            <div className="input-group mb-3">
-              <span className="input-group-text">₽</span>
-              <span className="input-group-text">0</span>
-              <input type="text" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
-            </div>
-            <div className="input-group">
-              <input type="text" className="form-control" aria-label="Dollar amount (with dot and two decimal places)" />
-              <span className="input-group-text">₽</span>
-              <span className="input-group-text">50000</span>
-            </div>
-
             <div className="col-8" />
           </div>
         </div>
