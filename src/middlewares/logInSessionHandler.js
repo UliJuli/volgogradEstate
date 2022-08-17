48ff95c3fa21a2990fe15f99d65403a25c2a95f5
IuriiSer <yusergeevi4@gmail.com>
2022-08-17 17:06:19 +0300

    add fix for advs

--- src/middlewares/logInSessionHandler.js
+++ src/middlewares/logInSessionHandler.js
@@ -1,13 +1,11 @@
 const logInSessionWrapper = (req, res, next) => {
   const user = req.session?.user;
   if (user) {
-    console.log('~ user', user);
     res.locals.user = user;
   }
 
   const admin = req.session?.admin;
   if (admin) {
-    console.log('~ admin', admin);
     res.locals.admin = admin;
   }
 
--- src/views/pages/AdminAdvsPage.jsx
+++ src/views/pages/AdminAdvsPage.jsx
@@ -3,13 +3,13 @@ const AdminPageDataContainer = require('../components/AdminPageDataContainer');
 const FilterForAdvs = require('../components/FilterForAdvs');
 const AdvsDrawer = require('../lib/AdvsDrawer');
 
-module.exports = function AdminAdvsPage({ title, rooms }) {
+module.exports = function AdminAdvsPage({ title, advs }) {
   return (
     <AdminPageDataContainer title={title}>
       <div className="container">
         <div className="d-flex flex-column">
           <FilterForAdvs />
-          <AdvsDrawer rooms={rooms} isForClient={false} />
+          <AdvsDrawer advs={advs} isForClient={false} />
         </div>
       </div>
     </AdminPageDataContainer>
