07614a40cc787cb8853d998f81b8a70488d3b397
Vlad <vladvasenin@gmail.com>
2022-08-17 14:51:25 +0300

    container

--- src/views/pages/MainPage.jsx
+++ src/views/pages/MainPage.jsx
@@ -3,7 +3,7 @@
 const React = require('react');
 const Layout = require('../components/Layout');
 
-module.exports = function Main({ title, user }) {
+module.exports = function Main({ title, user, rooms }) {
   return (
     <Layout user={user}>
       <div className="container">
@@ -19,18 +19,18 @@ module.exports = function Main({ title, user }) {
               <div className="checkbox form-check form-check-inline">
                 <h8>Количество комнат :</h8>
                 <div className="boxcheck">
-                <div className="form-check form-check-inline">
-                  <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
-                  <label className="form-check-label" htmlFor="inlineCheckbox1">1</label>
-                </div>
-                <div className="form-check form-check-inline">
-                  <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
-                  <label className="form-check-label" htmlFor="inlineCheckbox2">2</label>
-                </div>
-                <div className="form-check form-check-inline">
-                  <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
-                  <label className="form-check-label" htmlFor="inlineCheckbox3">3</label>
-                </div>
+                  <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1" />
+                    <label className="form-check-label" htmlFor="inlineCheckbox1">1</label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2" />
+                    <label className="form-check-label" htmlFor="inlineCheckbox2">2</label>
+                  </div>
+                  <div className="form-check form-check-inline">
+                    <input className="form-check-input" type="checkbox" id="inlineCheckbox3" value="option3" />
+                    <label className="form-check-label" htmlFor="inlineCheckbox3">3</label>
+                  </div>
                 </div>
               </div>
               <h8 className="aria-price">Диапозон цены:</h8>
@@ -47,7 +47,26 @@ module.exports = function Main({ title, user }) {
                 </div>
               </div>
             </div>
-            <div className="col-8" />
+            <div className="col-8">
+
+              {rooms.map((el) => (
+                <div className="main-container" key={el.id}>
+                  <div className="verical-container">
+                    <img src="#" alt="photo" />
+                    <h6>{el.title}</h6>
+                  </div>
+                  <div className="gorisont-container">
+                    <p className="category">{el.category}</p>
+                    <p className="price">{el.price}</p>
+                    <p className="square">{el.square}</p>
+                    <a href="#">Подробнее</a>
+                    <button type="button" className="btn btn-wishes">В избранное</button>
+                    <a href="#">На карте</a>
+
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
